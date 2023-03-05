b3245bdebdeaaf671f6a4bdb0ebf905eeb0e8691
Pedro Veras <veraspedro1608@gmail.com>
2023-03-05 17:15:29 -0300

    conexão com a api: controlled components

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,21 +3,24 @@ import { Overlay, Content, CloseButton, TransactionType, TransactionTypeButton }
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
 import * as z from 'zod'
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod'
 
 const newTransactionFormSchema = z.object({
     description: z.string(),
     price: z.number(),
     category: z.string(),
-    //type: z.enum(['income', 'outcome'])
+    type: z.enum(['income', 'outcome'])
 })
 
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
-export function NewTransactionModal(){
-    const { register, handleSubmit } = useForm<NewTransactionFormInputs>({
-        resolver: zodResolver(newTransactionFormSchema)
+export function NewTransactionModal() {
+    const { control, register, handleSubmit } = useForm<NewTransactionFormInputs>({
+        resolver: zodResolver(newTransactionFormSchema),
+        defaultValues: {
+            type: 'income'
+        }
     })
 
     function handleCreateNewTransaction(data: NewTransactionFormInputs) {
@@ -32,7 +35,7 @@ export function NewTransactionModal(){
                 <Dialog.Title>Nova Transação</Dialog.Title>
 
                 <CloseButton>
-                    <X size={24}/>
+                    <X size={24} />
                 </CloseButton>
 
                 <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
@@ -46,7 +49,7 @@ export function NewTransactionModal(){
                         type="number"
                         placeholder='Preço'
                         required
-                        {...register('price', { valueAsNumber: true})}
+                        {...register('price', { valueAsNumber: true })}
                     />
                     <input
                         type="text"
@@ -55,17 +58,30 @@ export function NewTransactionModal(){
                         {...register('category')}
                     />
 
-                    <TransactionType>
-                        <TransactionTypeButton variant='income' value='income'>
-                            <ArrowCircleUp size={24}/>
-                            Entrada
-                        </TransactionTypeButton>
+                    <Controller
+                        control={control}
+                        name="type"
+                        render={({ field }) => {
+                            console.log(field)
 
-                        <TransactionTypeButton variant='outcome' value='outcome'>
-                            <ArrowCircleDown size={24}/>
-                            Saída
-                        </TransactionTypeButton>
-                    </TransactionType>
+                            return (
+                                <TransactionType
+                                    onValueChange={field.onChange}
+                                    value={field.value}
+                                >
+                                    <TransactionTypeButton variant='income' value='income'>
+                                        <ArrowCircleUp size={24} />
+                                        Entrada
+                                    </TransactionTypeButton>
+
+                                    <TransactionTypeButton variant='outcome' value='outcome'>
+                                        <ArrowCircleDown size={24} />
+                                        Saída
+                                    </TransactionTypeButton>
+                                </TransactionType>
+                            )
+                        }}
+                    />
 
                     <button type='submit'>
                         Cadastrar
